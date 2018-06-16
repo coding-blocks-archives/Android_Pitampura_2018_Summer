c7de2907710b2221f054b14cc375bf75d67eb79a
Arnav Gupta <arnav@codingblocks.com>
2018-06-16 13:57:35 +0530

    lecture 1 and 2
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture01/Java_Sample/Hello.java
@@ -0,0 +1,5 @@
+class Hello {
+	public static void main (String[] kuchbhi) {
+		System.out.println("Hello World");
+	}
+}
---
+++ Lecture03/Layouts/app/src/main/java/com/codingblocks/layouts/MainActivity.java
@@ -0,0 +1,48 @@
+package com.codingblocks.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MAINACT";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: ");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: ");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: ");
+        super.onDestroy();
+    }
+}
