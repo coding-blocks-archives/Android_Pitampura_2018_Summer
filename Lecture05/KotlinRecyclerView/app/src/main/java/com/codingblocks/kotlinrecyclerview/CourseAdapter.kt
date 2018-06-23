7a04a806ac34e9077ca80e3b722a071ce10f052c
Arnav Gupta <arnav@codingblocks.com>
2018-06-23 13:20:45 +0530

    make all one line expressions
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture05/KotlinRecyclerView/app/src/main/java/com/codingblocks/kotlinrecyclerview/CourseAdapter.kt
+++ Lecture05/KotlinRecyclerView/app/src/main/java/com/codingblocks/kotlinrecyclerview/CourseAdapter.kt
@@ -13,25 +13,29 @@ class CourseAdapter (
 
     override fun getItemCount(): Int = courses.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
-        val li = parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val itemView = li.inflate(R.layout.list_item_course, parent, false)
-        return CourseViewHolder(itemView)
-    }
+    override fun onCreateViewHolder(
+            parent: ViewGroup, viewType: Int
+    ) = CourseViewHolder(
+            (parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
+                    .inflate(R.layout.list_item_course, parent, false)
+    )
 
-    override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
-        val course = courses[position]
-        holder.teacherName?.text = course.teacherName
-        holder.courseName?.text = course.name
-        holder.centerName?.text = course.centerName
-        holder.capacity?.text = course.capacity.toString()
-    }
 
-    class CourseViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
-        val teacherName = itemView?.tvTeacherName
-        val courseName = itemView?.tvCourseName
-        val centerName = itemView?.tvCenterName
-        val capacity = itemView?.tvCapacity
+
+    override fun onBindViewHolder(holder: CourseViewHolder, position: Int) = holder.bindData(courses[position])
+
+    class CourseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val teacherName = itemView.tvTeacherName
+        val courseName = itemView.tvCourseName
+        val centerName = itemView.tvCenterName
+        val capacity = itemView.tvCapacity
+
+        fun bindData (course: Course) {
+            teacherName.text = course.teacherName
+            courseName.text = course.name
+            centerName.text = course.centerName
+            capacity.text = course.capacity.toString()
+        }
     }
 
 }
