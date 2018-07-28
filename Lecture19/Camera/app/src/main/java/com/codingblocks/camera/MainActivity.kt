31c1f8eaa3840805ef5ae2045098352e96c9c912
Arnav Gupta <arnav@codingblocks.com>
2018-07-28 13:27:26 +0530

    set orientation of camera
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture19/Camera/app/src/main/java/com/codingblocks/camera/MainActivity.kt
+++ Lecture19/Camera/app/src/main/java/com/codingblocks/camera/MainActivity.kt
@@ -8,17 +8,35 @@ import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.util.Log
 import android.view.SurfaceHolder
+import android.view.Window
+import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
     lateinit var cam: Camera
+
     override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+
+        holder?.let { it.surface?.let {
+            try {
+                cam.stopPreview()
+            } catch (e: Exception) {}
+
+            try {
+                cam.setDisplayOrientation(90)
+                cam.setPreviewDisplay(holder)
+                cam.startPreview()
+            } catch (e: Exception) {}
+        }
+
+        }
     }
 
     override fun surfaceDestroyed(holder: SurfaceHolder?) {
     }
 
     override fun surfaceCreated(holder: SurfaceHolder?) {
+        cam.setDisplayOrientation(90)
         cam.setPreviewDisplay(svCameraPreview.holder)
         cam.startPreview()
     }
@@ -39,7 +57,7 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
         if (requestCode == 123) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                cam = Camera.open()
+                cam = Camera.open(0)
                 Log.d("CAM", """
 
                     Picture Size =
@@ -57,7 +75,9 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
                 }
 
                 svCameraPreview.holder.addCallback(this)
-
+                cam.setDisplayOrientation(90)
+                cam.setPreviewDisplay(svCameraPreview.holder)
+                cam.startPreview()
 
             }
         }
